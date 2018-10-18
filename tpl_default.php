0751ec253484f06208c74ac8ca60a145c5f880df
LarsGit223 <lars_paulsen@web.de>
2018-10-18 19:28:42 +0200

    Changed layout of config page in default layout.
    On entering the config page, the breadcrumbs and sidebar will be removed.
    The free space is used to render the table of contents in it instead of
    rendering it in the content area.

--- tpl_default.php
+++ tpl_default.php
@@ -172,6 +172,143 @@ $default_layout =
                     }
                 }
             ]
+        },
+        {
+            "select":["page==config"],
+            "top":"5vh",
+            "height":"90vh",
+            "background":"cicada-stripes",
+            "toc":"off-page",
+            "grid-vert-space":"20",
+            "grid":
+            [
+                [ "title",      "title",      "title",       "content", "content", "content", "content", "content", "content", "search" ],
+                [ "title",      "title",      "title",       "content", "content", "content", "content", "content", "content", "space" ],
+                [ "toc",        "toc",        "toc",         "content", "content", "content", "content", "content", "content", "scroll-up-area"      ],
+                [ "toc",        "toc",        "toc",         "content", "content", "content", "content", "content", "content", "pagetools"      ],
+                [ "toc",        "toc",        "toc",         "content", "content", "content", "content", "content", "content", "pagetools"      ],
+                [ "toc",        "toc",        "toc",         "content", "content", "content", "content", "content", "content", "pagetools"      ],
+                [ "toc",        "toc",        "toc",         "content", "content", "content", "content", "content", "content", "pagetools"      ],
+                [ "toc",        "toc",        "toc",         "content", "content", "content", "content", "content", "content", "pagetools"      ],
+                [ "toc",        "toc",        "toc",         "content", "content", "content", "content", "content", "content", "pagetools"      ],
+                [ "toc",        "toc",        "toc",         "content", "content", "content", "content", "content", "content", "scroll-down-area" ],
+                [ "toc",        "toc",        "toc",         "content", "content", "content", "content", "content", "content", "space"          ],
+                [ "cont-tools", "cont-tools", "cont-tools",  "content", "content", "content", "content", "content", "content", "space"          ],
+                [ "cont-tools", "cont-tools", "cont-tools",  "content", "content", "content", "content", "content", "content", "space"          ],
+                [ "cont-tools", "cont-tools", "cont-tools",  "content", "content", "content", "content", "content", "content", "space"          ],
+                [ "footer",     "footer",     "footer",      "footer",  "footer",  "footer",  "footer",  "footer",  "footer",  "space"         ]
+            ],
+            "cells":
+            [
+                {
+                    "id":"default",
+                    "border":"semi-transparent",
+                    "corners":"round"
+                },
+                {
+                    "id":"pagetools",
+                    "list-type":"no-text",
+                    "border":"semi-transparent",
+                    "corners":"round",
+                    "css":
+                    {
+                        "opacity":"0.5"
+                    }
+                },
+                {
+                    "id":"toc",
+                    "border":"semi-transparent",
+                    "corners":"round",
+                    "css":
+                    {
+                        "opacity":"0.75",
+                        "overflow-y":"scroll"
+                    }
+                },
+                {
+                    "id":"title",
+                    "border":"semi-transparent",
+                    "flex":
+                    {
+                        "direction":"row"
+                    },
+                    "css":
+                    {
+                        "background-color":"MediumSeaGreen",
+                        "color":"white",
+                        "text-shadow":"-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000",
+                        "opacity":"0.85"
+                    }
+                },
+                {
+                    "id":"cont-tools",
+                    "flex":
+                    {
+                        "direction":"column"
+                    },
+                    "items": ["usertools", "sitetools"],
+                    "border":"semi-transparent",
+                    "corners":"round",
+                    "css":
+                    {
+                        "opacity":"0.75"
+                    }
+                },
+                {
+                    "id":"scroll-up-area",
+                    "css":
+                    {
+                        "margin":"auto auto 10px auto",
+                        "opacity":"0.5",
+                        "height":"48px",
+                        "width":"48px"
+                    }
+                },
+                {
+                    "id":"scroll-down-area",
+                    "css":
+                    {
+                        "margin":"10px auto auto auto",
+                        "opacity":"0.5",
+                        "height":"48px",
+                        "width":"48px"
+                    }
+                },
+                {
+                    "id":"sitetools"
+                },
+                {
+                    "id":"usertools"
+                },
+                {
+                    "id":"search",
+                    "css":
+                    {
+                        "font-size":"1.25em",
+                        "background":"transparent",
+                        "margin":"auto 0",
+                        "width":"125%",
+                        "padding":"0 1em"
+                    }
+                },
+                {
+                    "id":"footer",
+                    "flex":
+                    {
+                        "direction":"column"
+                    },
+                    "border":"semi-transparent",
+                    "corners":"round",
+                    "css":
+                    {
+                        "margin":"auto 0",
+                        "font-size":"0.75em",
+                        "padding":"0.5em 2em",
+                        "text-align":"center",
+                        "opacity":"0.75"
+                    }
+                }
+            ]
         }
     ]
 }';
